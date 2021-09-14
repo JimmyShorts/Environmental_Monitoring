0388c7df7707a1ec18d8254233f467ae8a8129fe
Taylor <shulmantaylor4@gmail.com>
2021-09-14 05:16:51 -0700

    Make the scripts functions

--- Scripts/Data.py
+++ Scripts/Data.py
@@ -2,14 +2,15 @@ import pymssql
 import TestVars
 import Credentials
  
-server = ('192.168.1.14')
+server = Credentials.Server
 user = Credentials.DBuser
 password = Credentials.DBPass
 DataBaseName = Credentials.DB
 
 conn = pymssql.connect(server, user, password, DataBaseName)
 cursor = conn.cursor()
+
 #Will be an insert statement from using the variables pulled from humidity.py
-cursor.execute('Insert Into dbo.EMRecords(SensorNum, Temp, Humidity) Values(1,7,9)')
-conn.commit()
+cursor.execute('Insert Into dbo.EMRecords(SensorNum, Temp, Humidity) Values(%i,%i,%i)' % (TestVars.SensorNum, TestVars.Temperature, TestVars.Humidity))
+conn.commit() #need .commit() for inserts
 conn.close
